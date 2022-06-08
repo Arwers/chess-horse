362e46a59cf6a2db8f4340dc6205bb63152e3cdb
marcin.fortuna@onet.eu <marcin.fortuna@onet.eu>
2022-06-08 23:23:48 +0200

    + move function + root function

--- chessHorse.cpp
+++ chessHorse.cpp
@@ -5,6 +5,8 @@ typedef struct Points {
   int y;
 } Point;
 
+typedef enum { FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH, EIGHTH };
+
 int move( int** pChessBoard, int nDim, int move, int x, int y,
           int* px, int* py, Point* pHorseMoves );
 
@@ -19,6 +21,7 @@ void printChessBoard( int** pChessBoard, int nDim );
 int main( int argc, char* argv[] ) {
   int** p = createChessboard( 7 );
   printChessBoard( p, 7 );
+  freeChessboard( &p );
 }
 
 int** createChessboard( int nDim ) {
@@ -66,4 +69,46 @@ void freeChessboard( int*** pChessBoard ) {
   free( *pChessBoard );
   pChessBoard = NULL;
 }
+
+int move( int** pChessBoard, int nDim, int move, int x, int y,
+          int* px, int* py, Point* pHorseMoves ) {
+  
+  // nowe współrzędne
+  *px = x + pHorseMoves[ move ].x;
+  *py = y + pHorseMoves[ move ].y;
+
+  if( *px >= 0 && *px < nDim && *py >= 0 && *py < nDim ) { // sprawdzamy czy ruch jest poprawny
+    if( pChessBoard[ *px ][ *py ] == 0 ) return 1; // sprawdzamy czy byliśmy już w tym miejscu
+  }
+
+  return 0;
+}
+
+int root( int** pChessBoard, int nDim, int moveNo, int x, int y, Point* pHorseMoves ) {
+
+  // wstawic kolejny numer ruchu
+  pChessBoard[ x ][ y ] = moveNo;
+  // jesli koniec to zwracamy 1  (ilosc ruchow porownac z iloscia pozycji na szachownicy)
+  if( moveNo<=nDim*nDim ) return 1;
+  else {
+    // zdefiniowac nowe pozycje x i y
+    int xNew = 0;
+    int yNew = 0;
+    // sprawadzic wszystkie mozliwosci ruchu (petla od FIRST do EIGHTH)
+    for( int i = FIRST; i<=EIGHTH; i++ ) {
+      // jesli ruch do nowej poycji mozliwy
+      if( move( pChessBoard, CHESSBOARD_SIZE, i, x, y, &xNew, &yNew, pHorseMoves ) ) {
+        // to rekurencyjnie wykonac ruch moveNo+1
+        // jesli zwrocono 1 z rekurencji to zwrocic 1;
+        if( root( pChessBoard, CHESSBOARD_SIZE, moveNo+1, xNew, yNew, pHorseMoves ) ) return 1;
+      }
+    }
+  }
+  // jezeli wracamy to ustawiamy w pChessBoard[x][y] na 0 bo ten ruch byl zly
+  pChessBoard[ x ][ y ] = 0;
+  return 0;
+}
+
+
+
 
